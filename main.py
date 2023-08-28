3dca8b9514b19db4a802ca4c4c3b68f16f0fdbc7
Baron  Shao <shaobo9856@hotmail.com>
2023-08-28 19:06:20 +0800

    add usage limit

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from src.chatgpt import ChatGPT, DALLE
 from src.models import OpenAIModel
 from src.memory import Memory
 from src.server import keep_alive
+import datetime
+
+# 创建一个字典来存储用户的使用记录和限制信息
+user_records = {}
 
 load_dotenv()
 
@@ -31,8 +35,11 @@ def run():
             return
         await interaction.response.defer()
         try:
-            receive = chatgpt.get_response(user_id, message)
-            await sender.send_message(interaction, message, receive)
+            if check_user_limit(user_id, 10):
+                receive = chatgpt.get_response(user_id, message)
+                await sender.send_message(interaction, message, receive)
+            else:
+                await interaction.followup.send('> Reached the daily limit of 10 uses <')
         except Exception as e:
             logger.error(f"Error in chat: {e}")
             await interaction.followup.send('> Oops! Something went wrong. <')
@@ -61,6 +68,34 @@ def run():
     client.run(os.getenv('DISCORD_TOKEN'))
 
 
+def check_user_limit(user_id, limit):
+    # 获取当前日期
+    today = datetime.date.today()
+
+    # 检查用户ID是否存在于记录中
+    if user_id in user_records:
+        last_date, count = user_records[user_id]
+
+        # 检查最后一次使用的日期是否与当前日期相同
+        if last_date == today:
+            # 如果日期相同，将使用次数加1
+            count += 1
+        else:
+            # 如果日期不同，将使用次数重置为1
+            count = 1
+    else:
+        # 如果用户ID不存在于记录中，将其添加到记录中，并将使用次数设置为1
+        count = 1
+
+    # 更新用户记录
+    user_records[user_id] = (today, count)
+
+    # 检查使用次数是否超过限制
+    if count > limit:
+        return False
+    else:
+        return True
+    
 if __name__ == '__main__':
     keep_alive()
     run()
